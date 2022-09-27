a8aeca18f86e244832f93372f7b23bcbf434ebc3
lbnv13 <dimanml67@gmail.com>
2022-09-28 00:43:50 +0300

    HW-04 cloud-testapp

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+sudo apt-get install apt-transport-https ca-certificates
+sudo apt update
+sudo apt upgrade -y
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
 echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
 sudo apt-get update
