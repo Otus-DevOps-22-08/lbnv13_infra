046891eeb176d63d07fc41f84c7d0356a81d6b49
lbnv13 <lbnv13>
2022-09-28 09:00:02 +0300

    HW-04 cloud-testapp

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo apt install git
-cd /home/yc-user && git clone -b monolith https://github.com/express42/reddit.git
+git clone -b monolith https://github.com/express42/reddit.git
 cd reddit && bundle install
 puma -d
