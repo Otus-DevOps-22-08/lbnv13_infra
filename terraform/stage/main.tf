5a82020bdb803766802d6bcf1fc46f1b5a44422c
lbnv13 <dimanml67@gmail.com>
2022-11-06 22:55:24 +0300

    HW-07 Terraform-2

--- terraform/stage/main.tf
+++ terraform/stage/main.tf
@@ -1,10 +1,10 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#}
 
 provider "yandex" {
   service_account_key_file = var.service_account_key_file
