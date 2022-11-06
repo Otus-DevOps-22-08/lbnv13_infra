d85dca7f25146c0b4fc305598fe7a204bb89b765
lbnv13 <dimanml67@gmail.com>
2022-11-06 23:00:02 +0300

    HW-07 Terraform-2

--- terraform/modules/db/db.tf
+++ terraform/modules/db/db.tf
@@ -1,10 +1,10 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-}
+#terraform {
+#  required_providers {
+#    yandex = {
+#      source = "yandex-cloud/yandex"
+#    }
+#  }
+#}
 
 resource "yandex_compute_instance" "db" {
   name = "reddit-db"
